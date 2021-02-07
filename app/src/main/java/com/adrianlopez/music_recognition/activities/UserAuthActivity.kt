e22446083012bdfd60d01cb6d61fc9e83a19588f
adrian_lopez15 <lopezhadrian10@gmail.com>
2021-02-07 12:07:57 -0800

    UserAuthActivity.kt created LoginFragment.kt created Basic Email/Password Login implemented. android:windowSoftInputMode="adjustResize" added to AndroidManifest.xml to avoid views being cover by keyboard Firebase SDK Implemented

---
+++ app/src/main/java/com/adrianlopez/music_recognition/activities/UserAuthActivity.kt
@@ -0,0 +1,12 @@
+package com.adrianlopez.music_recognition.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adrianlopez.music_recognition.R
+
+class UserAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_auth)
+    }
+}
---
+++ app/src/main/java/com/adrianlopez/music_recognition/viewmodels/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.adrianlopez.music_recognition.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
